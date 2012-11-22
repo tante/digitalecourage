af8087d3a1fc0e43aa16ac9c935440df6e8df5b8
Jürgen Geuter <tante@the-gay-bar.com>
2012-11-22 12:11:18 +0100

    Skeleton und style

--- index.php
+++ index.php
@@ -5,8 +5,18 @@ $quotes = file("quotes.txt");
 
 // pick random quote
 $index = rand(0,count($quotes)-1);
-
-print("Habe Mut, dich ".$quotes[$index]);
+?>
+<html>
+    <head>
+        <title>Digitale Courage</title>
+        <link href='http://fonts.googleapis.com/css?family=Nunito' rel='stylesheet' type='text/css'>
+        <link href="style.css" rel="stylesheet" type="text/css">
+   </head>
+   <body>
+      <div id="distance"></div>
+      <div id="text">"Habe Mut, dich <?= $quotes[$index]?>"</div>
+   </body>
+</html>
 
 
 ?>
